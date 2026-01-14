ba2944bd4ffccb1ece0ccc519c69bc6dde913f0a
freddyolmos <fidelolmos334@gmail.com>
2026-01-13 19:56:27 -0600

    Conexion exitosa con Prisma.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: '*',
+  });
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`🚀 API running on http://localhost:${port}/api`);
 }
 bootstrap();
